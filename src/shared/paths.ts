0c62bfa19c4d37b2f6192817a32af6308cce9c7d
Neo (Guido) <31198370+GuidoTapia@users.noreply.github.com>
2021-10-16 08:15:00 -0500

    Feature: Added general router (#2)
    * Feature: Added General Sidebar
    
    * Feature: Added general router

---
+++ src/shared/paths.ts
@@ -0,0 +1,17 @@
+export const paths = {
+    root: '/',
+    error: '/error',
+    general: {
+        home: '/home',
+        about: '/about',
+        validate: '/validate-token',
+        notFound: '/not-found',
+        styleSamples: '/style-samples',
+      },
+    vehicles: {
+      default: '/vehicles',
+      create: '/vehicles/create',
+      update: '/vehicles/update',
+      delete: '/vehicles/delete',
+    },
+  }
