98db2de0a7abdb028c93f490fac10b436f54067d
Guido Tapia <guido.tapia@ucsp.edu.pe>
2021-10-16 07:21:17 -0500

    Feature: Added vehicles view and integration to database

---
+++ src/screens/Vehicles/CreateVehicle/CreateVehicle.tsx
@@ -0,0 +1,7 @@
+import 'antd/dist/antd.css';
+
+export const CreateVehicle: React.FC = () => {
+  return (
+    <div>Create Vehicle</div>
+  );
+}
--- src/shared/AppRouter/AppRouter.tsx
+++ src/shared/AppRouter/AppRouter.tsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Sidebar } from '../../components/Sidebar'
+import { CreateVehicle } from '../../screens/Vehicles/CreateVehicle/CreateVehicle'
+import { DeleteVehicle } from '../../screens/Vehicles/DeleteVehicle/DeleteVehicle'
+import { UpdateVehicle } from '../../screens/Vehicles/UpdateVehicle/UpdateVehicle'
 import { Vehicles } from '../../screens/Vehicles/Vehicles'
 import AppSwitch from '../AppSwitch'
 import { paths } from '../paths'
@@ -15,9 +18,15 @@ export const AppRouter: React.FC = () => {
       <Route exact path={paths.general.styleSamples} component={()=><div>Style</div>} />
       <Route exact path={paths.general.validate} component={()=><div>Validate</div>} />
 
-        <Sidebar headerName="Vehicles">
-      <Route exact path={paths.general.home} component={()=><div>Home</div>} />
+        
+      <Sidebar headerName="Vehículos">
       <Route exact path={paths.vehicles.default} component={Vehicles} />
+      <Route exact path={paths.vehicles.create} component={CreateVehicle} />
+      <Route exact path={paths.vehicles.update} component={UpdateVehicle} />
+      <Route exact path={paths.vehicles.delete} component={DeleteVehicle} />
+      </Sidebar>
+      <Sidebar headerName="Home">
+      <Route exact path={paths.general.home} component={()=><div>Home</div>} />
       </Sidebar>
     </AppSwitch>
     </BrowserRouter>
