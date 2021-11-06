eb5a174064b9b2127ffde6a7bba5a6190f71cc98
Guido Tapia <guido.tapia@ucsp.edu.pe>
2021-11-06 08:21:21 -0500

    CU-03 Registrar un nuevo vehículo

--- src/screens/Vehicles/CreateVehicle/CreateVehicle.tsx
+++ src/screens/Vehicles/CreateVehicle/CreateVehicle.tsx
@@ -1,132 +1,131 @@
-import { Button, Col, Input, Row, Form } from 'antd';
-import 'antd/dist/antd.css';
-import {Paper} from '../../../components/Paper'
-import styled from 'styled-components';
-import { useVehicles } from '../shared/VehiclesContext/VehiclesContext';
-import { IVehiclesToApi } from '../../../shared/api';
-import { useHistory } from 'react-router-dom';
+//CU-03 Registrar un nuevo vehículo
+import { Button, Col, Input, Row, Form } from "antd";
+import "antd/dist/antd.css";
+import { Paper } from "../../../components/Paper";
+import styled from "styled-components";
+import { useVehicles } from "../shared/VehiclesContext/VehiclesContext";
+import { IVehiclesToApi } from "../../../shared/api";
+import { useHistory } from "react-router-dom";
 
 export const CreateVehicle: React.FC = () => {
-const {createVehicle}=useVehicles()
-const history = useHistory();
-  const onSend=(event:IVehiclesToApi)=>{
-    createVehicle(event)
+  const { createVehicle } = useVehicles();
+  const history = useHistory();
+  const onSend = (event: IVehiclesToApi) => {
+    createVehicle(event);
     history.push("/vehicles");
-  }
-    return (
+  };
+  return (
     <Paper>
-        <h1>Ingresa los Datos del Nuevo Vehículo</h1>
-        <Form
-      name="basic"
-      layout="vertical"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onSend}
-      autoComplete="off"
-    >
+      <h1>Ingresa los Datos del Nuevo Vehículo</h1>
+      <Form
+        name="basic"
+        layout="vertical"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onSend}
+        autoComplete="off"
+      >
         <Row>
-      <Col span={12} style={{padding:'8px 16px'}}>
-      <Form.Item
-        label="Propietario"
-        name="usuario"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-      <Col span={12} style={{padding:'8px 16px'}}>
-      <Form.Item
-        label="Placa"
-        name="placa"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-      </Row>
-      <Row>
-      <Col span={12} style={{padding:'8px 16px'}}>
-      <Form.Item
-        label="Año de Fabricación"
-        name="anho"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-      <Col span={12} style={{padding:'8px 16px'}}>
-      <Form.Item
-        label="Categoría"
-        name="categoria"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-      </Row>
-      <Row>
-      <Col span={12} style={{padding:'8px 16px'}}>
-      <Form.Item
-        label="Marca"
-        name="marca"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-      <Col span={12} style={{padding:'8px 16px'}}>
-      <Form.Item
-        label="Tipo de Unidad"
-        name="unidad"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-      </Row>
-      <Form.Item>
-      <StyledBottomButtons>
-    <Button type="primary" htmlType="submit">Registrar Vehiculo</Button>
-    </StyledBottomButtons>
-      </Form.Item>
+          <Col span={12} style={{ padding: "8px 16px" }}>
+            <Form.Item
+              label="Propietario"
+              name="usuario"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12} style={{ padding: "8px 16px" }}>
+            <Form.Item
+              label="Placa"
+              name="placa"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12} style={{ padding: "8px 16px" }}>
+            <Form.Item
+              label="Año de Fabricación"
+              name="anho"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12} style={{ padding: "8px 16px" }}>
+            <Form.Item
+              label="Categoría"
+              name="categoria"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12} style={{ padding: "8px 16px" }}>
+            <Form.Item
+              label="Marca"
+              name="marca"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12} style={{ padding: "8px 16px" }}>
+            <Form.Item
+              label="Tipo de Unidad"
+              name="unidad"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item>
+          <StyledBottomButtons>
+            <Button type="primary" htmlType="submit">
+              Registrar Vehiculo
+            </Button>
+          </StyledBottomButtons>
+        </Form.Item>
       </Form>
-        
-        
-        
-   
     </Paper>
   );
-}
+};
 
 const StyledBottomButtons = styled.div`
-margin-top: 24px;
-  display:flex;
+  margin-top: 24px;
+  display: flex;
   flex-direction: row;
   justify-content: flex-end;
   margin-bottom: 24px;
-`
+`;
--- src/screens/Vehicles/Vehicles.tsx
+++ src/screens/Vehicles/Vehicles.tsx
@@ -1,80 +1,92 @@
-
-import { Button, Table } from 'antd';
-import 'antd/dist/antd.css';
-import styled from 'styled-components';
+import { Button, Table } from "antd";
+import "antd/dist/antd.css";
+import styled from "styled-components";
 import { useHistory } from "react-router-dom";
-import { useVehicles } from './shared/VehiclesContext/VehiclesContext';
-import { IVehicles } from '../../shared/api';
-import { DeleteVehicle } from './DeleteVehicle/DeleteVehicle';
-//"placa" : "ABC-123", "categoria" : "M1", "usuario" : "SINNENS", "unidad" : "Combi", "anho" : "2010" 
-  
-  const columns = [
-    {
-      title: 'Placa',
-      dataIndex: 'placa',
-      key: 'placa',
-    },
-    {
-      title: 'Año de Fabricación',
-      dataIndex: 'anho',
-      key: 'anho',
-    },
-    {
-      title: 'Tipo de Unidad',
-      dataIndex: 'unidad',
-      key: 'unidad',
-    },
-    {
-      title: 'Categoría',
-      dataIndex: 'categoria',
-      key: 'categoria',
-    },
-  ];
-  
-export const Vehicles: React.FC = () => {
-  const {vehicles,selectVehicle,vehicleSelected}=useVehicles(); 
-  
-  const history = useHistory();
+import { useVehicles } from "./shared/VehiclesContext/VehiclesContext";
+import { IVehicles } from "../../shared/api";
+import { DeleteVehicle } from "./DeleteVehicle/DeleteVehicle";
+//"placa" : "ABC-123", "categoria" : "M1", "usuario" : "SINNENS", "unidad" : "Combi", "anho" : "2010"
 
+const columns = [
+  {
+    title: "Placa",
+    dataIndex: "placa",
+    key: "placa",
+  },
+  {
+    title: "Año de Fabricación",
+    dataIndex: "anho",
+    key: "anho",
+  },
+  {
+    title: "Tipo de Unidad",
+    dataIndex: "unidad",
+    key: "unidad",
+  },
+  {
+    title: "Categoría",
+    dataIndex: "categoria",
+    key: "categoria",
+  },
+];
+
+export const Vehicles: React.FC = () => {
+  const { vehicles, selectVehicle, vehicleSelected } = useVehicles();
+
+  const history = useHistory();
+  //CU-03 Registrar un nuevo vehículo
   function newVehicle() {
     history.push("/vehicles/create");
   }
   function updateVehicle() {
     history.push("/vehicles/update");
   }
-  function selectRow(selectedRowKeys:React.Key[],selectedRows:IVehicles[]) {
-    selectVehicle(selectedRows[0])
+  function selectRow(selectedRowKeys: React.Key[], selectedRows: IVehicles[]) {
+    selectVehicle(selectedRows[0]);
   }
   return (
     <>
-    <StyledUpperButtons>
-    <Button type="primary" onClick={newVehicle}>Nuevo Vehiculo</Button>
-    </StyledUpperButtons>
-    <Table rowSelection={{type:'radio',onChange:selectRow}} dataSource={vehicles} columns={columns} />
-    {vehicleSelected?
-    <StyledBottomButtons>
-    <DeleteVehicle/>
-    <Button type="default" onClick={updateVehicle}>Editar Vehiculo</Button>
-    </StyledBottomButtons>:
-    <StyledBottomButtons>
-    <Button type="dashed" disabled>Eliminar Vehiculo</Button>
-    <Button type="dashed" disabled>Editar Vehiculo</Button>
-    </StyledBottomButtons>
-}
-  </>
+      <StyledUpperButtons>
+        <Button type="primary" onClick={newVehicle}>
+          Nuevo Vehiculo
+        </Button>
+      </StyledUpperButtons>
+      <Table
+        rowSelection={{ type: "radio", onChange: selectRow }}
+        dataSource={vehicles}
+        columns={columns}
+      />
+      {vehicleSelected ? (
+        <StyledBottomButtons>
+          <DeleteVehicle />
+          <Button type="default" onClick={updateVehicle}>
+            Editar Vehiculo
+          </Button>
+        </StyledBottomButtons>
+      ) : (
+        <StyledBottomButtons>
+          <Button type="dashed" disabled>
+            Eliminar Vehiculo
+          </Button>
+          <Button type="dashed" disabled>
+            Editar Vehiculo
+          </Button>
+        </StyledBottomButtons>
+      )}
+    </>
   );
-}
+};
 
 const StyledUpperButtons = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: row;
   justify-content: flex-end;
   margin-bottom: 24px;
-`
+`;
 const StyledBottomButtons = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: row;
   justify-content: flex-end;
   margin-bottom: 24px;
   gap: 16px;
-`
+`;
