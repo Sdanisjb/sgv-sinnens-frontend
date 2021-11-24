3eb4fb90bbf663e3ae9ce8c474a28b94282770b5
Neo <31198370+GuidoTapia@users.noreply.github.com>
2021-11-24 09:57:46 -0500

    Feature: Added charts for vehicles

--- src/screens/Vehicles/Vehicles.tsx
+++ src/screens/Vehicles/Vehicles.tsx
@@ -1,10 +1,14 @@
-import { Button, Table } from "antd";
+import { Button, Checkbox, Divider, Switch, Table } from "antd";
 import "antd/dist/antd.css";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { useVehicles } from "./shared/VehiclesContext/VehiclesContext";
 import { IVehicles } from "../../shared/api";
 import { DeleteVehicle } from "./DeleteVehicle/DeleteVehicle";
+import ReactApexCharts from "react-apexcharts";
+import { useEffect, useState } from "react";
+import { CheckboxValueType } from "antd/lib/checkbox/Group";
+import { CheckboxChangeEvent } from "antd/lib/checkbox";
 //"placa" : "ABC-123", "tipo" : "M1", "usuario" : "SINNENS", "unidad" : "Combi", "anho" : "2010"
 
 const columns = [
@@ -12,11 +16,43 @@ const columns = [
     title: "Placa",
     dataIndex: "placa",
     key: "placa",
+    sorter: (a: IVehicles, b: IVehicles) =>
+      a.placa.charCodeAt(0) * 10000 -
+      b.placa.charCodeAt(0) * 10000 +
+      (a.placa.charCodeAt(1) * 100 - b.placa.charCodeAt(1) * 100) +
+      (a.placa.charCodeAt(2) - b.placa.charCodeAt(2)),
   },
   {
     title: "Año de Fabricación",
     dataIndex: "anho",
     key: "anho",
+    sorter: (a: IVehicles, b: IVehicles) => a.anho - b.anho,
+    filters: [
+      {
+        text: "2020-2029",
+        value: 2025,
+      },
+      {
+        text: "2010-2019",
+        value: 2015,
+      },
+      {
+        text: "2000-2009",
+        value: 2005,
+      },
+      {
+        text: "1990-1999",
+        value: 1995,
+      },
+      {
+        text: "1980-1990",
+        value: 1985,
+      },
+    ],
+    onFilter: (value: string | number | boolean, record: IVehicles) =>
+      typeof value === "number"
+        ? record.anho >= value - 5 && record.anho < value + 5
+        : false,
   },
   {
     title: "Tipo de Unidad",
@@ -27,17 +63,105 @@ const columns = [
     title: "Tipo",
     dataIndex: "tipo",
     key: "tipo",
+    sorter: (a: IVehicles, b: IVehicles) =>
+      a.tipo.charCodeAt(0) * 100 -
+      b.tipo.charCodeAt(0) * 100 +
+      a.tipo.charCodeAt(1) -
+      b.tipo.charCodeAt(1),
+    filters: [
+      {
+        text: "M1",
+        value: "M1",
+      },
+      {
+        text: "M2",
+        value: "M2",
+      },
+      {
+        text: "M3",
+        value: "M3",
+      },
+      {
+        text: "N1",
+        value: "N1",
+      },
+      {
+        text: "N2",
+        value: "N2",
+      },
+      {
+        text: "N3",
+        value: "N3",
+      },
+    ],
+    onFilter: (value: string | number | boolean, record: IVehicles) =>
+      typeof value === "string" ? record.tipo.indexOf(value) === 0 : false,
   },
   {
     title: "Propietario",
     dataIndex: "usuario",
     key: "usuario",
+    sorter: (a: IVehicles, b: IVehicles) =>
+      a.usuario.charCodeAt(0) * 10000 -
+      b.usuario.charCodeAt(0) * 10000 +
+      (a.usuario.charCodeAt(1) * 100 - b.usuario.charCodeAt(1) * 100) +
+      (a.usuario.charCodeAt(2) - b.usuario.charCodeAt(2)),
+    filters: [
+      {
+        text: "SINNENS",
+        value: "SINNENS",
+      },
+      {
+        text: "CERRO VERDE",
+        value: "CERRO VERDE",
+      },
+    ],
+    onFilter: (value: string | number | boolean, record: IVehicles) =>
+      typeof value === "string" ? record.usuario.indexOf(value) === 0 : false,
   },
 ];
 
+const plainCheckOptions = ["Año", "Tipo de Unidad", "Tipo", "Propietario"];
+const defaultCheckedList = ["Año"];
+
 export const Vehicles: React.FC = () => {
   const { vehicles, selectVehicle, vehicleSelected } = useVehicles();
+  const [showCharts, setShowCharts] = useState(false);
+  const [checkedList, setCheckedList] =
+    useState<CheckboxValueType[]>(defaultCheckedList);
+  const [indeterminate, setIndeterminate] = useState(true);
+  const [checkAll, setCheckAll] = useState(false);
+  const [seriesYear, setSeriesYear] = useState<{ [key: string]: number }>({});
+  const [seriesUnidad, setSeriesUnidad] = useState<{ [key: string]: number }>(
+    {}
+  );
+  const [seriesTipo, setSeriesTipo] = useState<{ [key: string]: number }>({});
+  const [seriesUser, setSeriesUser] = useState<{ [key: string]: number }>({});
 
+  useEffect(() => {
+    const auxSeriesYear = seriesYear;
+    const auxSeriesUnidad = seriesUnidad;
+    const auxSeriesTipo = seriesTipo;
+    const auxSeriesUser = seriesUser;
+    vehicles.forEach((vehicle) => {
+      const anho = vehicle.anho.toString();
+      const { unidad, tipo, usuario } = vehicle;
+
+      console.log(anho);
+      if (auxSeriesYear[anho]) auxSeriesYear[anho] += 1;
+      else auxSeriesYear[anho] = 1;
+      if (auxSeriesUnidad[unidad]) auxSeriesUnidad[unidad] += 1;
+      else auxSeriesUnidad[unidad] = 1;
+      if (auxSeriesTipo[tipo]) auxSeriesTipo[tipo] += 1;
+      else auxSeriesTipo[tipo] = 1;
+      if (auxSeriesUser[usuario]) auxSeriesUser[usuario] += 1;
+      else auxSeriesUser[usuario] = 1;
+    });
+    setSeriesYear(auxSeriesYear);
+    setSeriesUnidad(auxSeriesUnidad);
+    setSeriesTipo(auxSeriesTipo);
+    setSeriesUser(auxSeriesUser);
+  }, [vehicles]);
   const history = useHistory();
   //CU-03 Registrar un nuevo vehículo
   function newVehicle() {
@@ -50,35 +174,149 @@ export const Vehicles: React.FC = () => {
   function selectRow(selectedRowKeys: React.Key[], selectedRows: IVehicles[]) {
     selectVehicle(selectedRows[0]);
   }
+  function onChangeSwitch(checked: boolean) {
+    setShowCharts(checked);
+  }
+  const onChangeCheck = (checkedValue: CheckboxValueType[]) => {
+    setCheckedList(checkedValue);
+    setIndeterminate(
+      !!checkedValue.length && checkedValue.length < plainCheckOptions.length
+    );
+    setCheckAll(checkedValue.length === plainCheckOptions.length);
+  };
+
+  const onCheckAllChange = (e: CheckboxChangeEvent) => {
+    setCheckedList(e.target.checked ? plainCheckOptions : []);
+    setIndeterminate(false);
+    setCheckAll(e.target.checked);
+  };
   return (
     <>
       <StyledUpperButtons>
         <Button type="primary" onClick={newVehicle}>
           Nuevo Vehiculo
         </Button>
+        <StyledRightUpperButtons>
+          <h4>Mostrar:</h4>
+          <Switch
+            checkedChildren="Gráficos"
+            unCheckedChildren="Tablas"
+            onChange={onChangeSwitch}
+          />
+        </StyledRightUpperButtons>
       </StyledUpperButtons>
-      <Table
-        rowSelection={{ type: "radio", onChange: selectRow }}
-        dataSource={vehicles}
-        columns={columns}
-      />
-      {vehicleSelected ? (
-        <StyledBottomButtons>
-          {/* CU-05 Borrar Vehículo */}
-          <DeleteVehicle />
-          <Button type="default" onClick={updateVehicle}>
-            Editar Vehiculo
-          </Button>
-        </StyledBottomButtons>
+      {showCharts ? (
+        <>
+          <h3>Mostrar Grafico Resumen de:</h3>
+          <Checkbox.Group
+            options={plainCheckOptions}
+            value={checkedList}
+            onChange={onChangeCheck}
+          />
+          <Checkbox
+            indeterminate={indeterminate}
+            onChange={onCheckAllChange}
+            checked={checkAll}
+          >
+            Seleccionar Todos
+          </Checkbox>
+          <StyledChartGroup>
+            {checkedList.includes("Año") && (
+              <div>
+                <h4>Año de Fabricación:</h4>
+                <ReactApexCharts
+                  options={{
+                    chart: {
+                      width: 440,
+                      type: "donut",
+                    },
+                    labels: Object.keys(seriesYear),
+                  }}
+                  series={Object.values(seriesYear)}
+                  type="donut"
+                  width={440}
+                />
+              </div>
+            )}
+            {checkedList.includes("Tipo") && (
+              <div>
+                <h4>Tipo:</h4>
+                <ReactApexCharts
+                  options={{
+                    chart: {
+                      width: 440,
+                      type: "donut",
+                    },
+                    labels: Object.keys(seriesTipo),
+                  }}
+                  series={Object.values(seriesTipo)}
+                  type="donut"
+                  width={440}
+                />
+              </div>
+            )}
+            {checkedList.includes("Tipo de Unidad") && (
+              <div>
+                <h4>Tipo de Unidad:</h4>
+                <ReactApexCharts
+                  options={{
+                    chart: {
+                      width: 440,
+                      type: "donut",
+                    },
+                    labels: Object.keys(seriesUnidad),
+                  }}
+                  series={Object.values(seriesUnidad)}
+                  type="donut"
+                  width={440}
+                />
+              </div>
+            )}
+            {checkedList.includes("Propietario") && (
+              <div>
+                <h4>Propietario:</h4>
+                <ReactApexCharts
+                  options={{
+                    chart: {
+                      width: 440,
+                      type: "donut",
+                    },
+                    labels: Object.keys(seriesUser),
+                  }}
+                  series={Object.values(seriesUser)}
+                  type="donut"
+                  width={440}
+                />
+              </div>
+            )}
+          </StyledChartGroup>
+        </>
       ) : (
-        <StyledBottomButtons>
-          <Button type="dashed" disabled>
-            Eliminar Vehiculo
-          </Button>
-          <Button type="dashed" disabled>
-            Editar Vehiculo
-          </Button>
-        </StyledBottomButtons>
+        <>
+          <Table
+            rowSelection={{ type: "radio", onChange: selectRow }}
+            dataSource={vehicles}
+            columns={columns}
+          />
+          {vehicleSelected ? (
+            <StyledBottomButtons>
+              {/* CU-05 Borrar Vehículo */}
+              <DeleteVehicle />
+              <Button type="default" onClick={updateVehicle}>
+                Editar Vehiculo
+              </Button>
+            </StyledBottomButtons>
+          ) : (
+            <StyledBottomButtons>
+              <Button type="dashed" disabled>
+                Eliminar Vehiculo
+              </Button>
+              <Button type="dashed" disabled>
+                Editar Vehiculo
+              </Button>
+            </StyledBottomButtons>
+          )}
+        </>
       )}
     </>
   );
@@ -87,8 +325,13 @@ export const Vehicles: React.FC = () => {
 const StyledUpperButtons = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: space-between;
   margin-bottom: 24px;
+`;
+const StyledRightUpperButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
 `;
 const StyledBottomButtons = styled.div`
   display: flex;
@@ -96,4 +339,11 @@ const StyledBottomButtons = styled.div`
   justify-content: flex-end;
   margin-top: 24px;
   gap: 16px;
+`;
+const StyledChartGroup = styled.div`
+  margin-top: 24px;
+  gap: 40px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
